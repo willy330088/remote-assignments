68b87008b68818f611105f830e411495d50657bf
Williamlin308 <williamlin330088@gmail.com>
2021-08-04 09:42:35 +0800

    change var to const

--- week-2/js-assignments/Assignment-1.js
+++ week-2/js-assignments/Assignment-1.js
@@ -1,6 +1,6 @@
 function max(numbers) {
     let max = numbers[0];
-    for (var i = 0; i < numbers.length; i++) {
+    for (const i = 0; i < numbers.length; i++) {
         if (numbers[i] >= max) {
             max = numbers[i];
         }
--- week-2/js-assignments/Assignment-5.js
+++ week-2/js-assignments/Assignment-5.js
@@ -1,6 +1,6 @@
 function twoSum(nums, target) {
-    for (var i = 0; i < nums.length; i++) {
-        for (var j = i + 1; j < nums.length; j++) {
+    for (const i = 0; i < nums.length; i++) {
+        for (const j = i + 1; j < nums.length; j++) {
             if (nums[j] == target - nums[i]) {
                 return [i, j]
             }
@@ -11,7 +11,7 @@ function twoSum(nums, target) {
 // One-pass Hash Table
 function twosum(nums, target) {
     let map = new Map;
-    for (var i = 0; i < nums.length; i++) {
+    for (const i = 0; i < nums.length; i++) {
         let complement = target - nums[i];
         if (map.has(complement)) {
             return [map.get(complement), i]

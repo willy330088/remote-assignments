18aa0567162c32c355ed557d86b2891eb2a36a58
Williamlin308 <williamlin330088@gmail.com>
2021-08-12 10:51:49 +0800

    modify AJAX assignment

--- week-3/routes/getData.js
+++ week-3/routes/getData.js
@@ -4,16 +4,18 @@ const router = express.Router();
 router.get('/', (req, res) => {
     let answer;
     let wrong;
+    let targetNumber;
 
-    if (req.query.number == 'xyz') {
-        answer = 'xyz';
+    if (isNaN(req.query.number) || parseInt(req.query.number) < 0) {
+        answer = true;
         wrong = true;
     } else {
         req.query.number = parseInt(req.query.number);
-        const { number } = req.query;
-        answer = (1 + number) * number / 2;
+        targetNumber = req.query.number;
+        answer = (1 + targetNumber) * targetNumber / 2;
     }
-    res.render('getData', { answer, wrong });
+
+    res.render('getData', { answer, wrong, targetNumber });
 });
 
 module.exports = router;
